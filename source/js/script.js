149f7946c788801bbc7cbaad825916bb9b99da61
OPNAUMOV <asler-72@mail.ru>
2020-12-19 02:15:45 +0300

    новые правки

--- source/js/script.js
+++ source/js/script.js
@@ -3,9 +3,6 @@ const closeMenuButton = document.querySelector(".page-navigation__toggle-close")
 const staticElements = document.querySelector(".page-navigation__static-elements");
 const mainElements = document.querySelector(".page-navigation__main-elements");
 
-$(function () {
-  $('.staticElements').addClass("hpage-navigation__hide");
-});
 
 menuButton.addEventListener("click", function(evt) {
   evt.preventDefault();
